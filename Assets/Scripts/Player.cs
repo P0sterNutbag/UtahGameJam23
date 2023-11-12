9a3fbd58b641975b9e94811370821ef9e2348f00
Adam Allred <AdamAllred64@gmail.com>
2023-11-11 19:53:25 -0700

    Adding scoring and player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int alligence;
+
+    private void Start()
+    {
+        alligence = 50;
+    }
+
+
+}
