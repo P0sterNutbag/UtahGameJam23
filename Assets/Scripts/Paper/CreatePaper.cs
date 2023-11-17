76ab9bcdfcd1f99597fecc6c32591c731e820975
Adam Allred <AdamAllred64@gmail.com>
2023-11-17 15:43:57 -0700

    Fixed the boxCollider2d of DropBox

--- Assets/Scripts/Paper/CreatePaper.cs
+++ Assets/Scripts/Paper/CreatePaper.cs
@@ -4,23 +4,24 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using UnityEngine.Windows;
+using UnityEngine.UIElements;
 
 public class CreatePaper : MonoBehaviour
 {
     public TextMeshPro textMeshPro;
-    public string targetWord;
+    public List<string> targetWords = new List<string>();
     public GameObject dropBox;
     string pattern;
 
     void Start()
     {
-        string pattern = @"\[([^\]]+)\]";
+        pattern = @"\[([^\]]+)\]";
         StartCoroutine(LateStart(0.1f));
 
 
     }
 
-    private void GetWordLocations()
+    private void GetWordLocation(string word)
     {
         if (textMeshPro == null)
         {
@@ -28,26 +29,16 @@ public class CreatePaper : MonoBehaviour
             return;
         }
 
-        // Find the index of the target word in the text
-        int wordIndex = textMeshPro.text.IndexOf(targetWord);
+        int index = textMeshPro.text.IndexOf(word);
 
-        // Ensure the target word exists in the text
-        if (wordIndex != -1)
-        {
-            // Get the RectTransform of the text
-            RectTransform rectTransform = textMeshPro.rectTransform;
+        // Get the RectTransform of the text
+        RectTransform rectTransform = textMeshPro.rectTransform;
+        CreateDropBox(rectTransform, index, word.Length, word);
 
-            // Get the RectTransform of the target word
-            RectTransform wordRectTransform = GetWordRectTransform(rectTransform, wordIndex, targetWord.Length);
-        }
-        else
-        {
-            Debug.LogError("Target word not found in the TextMeshPro text.");
-        }
     }
 
     // Helper function to get the RectTransform of a portion of the TextMeshPro text
-    RectTransform GetWordRectTransform(RectTransform parentRectTransform, int startIndex, int length)
+    private void CreateDropBox(RectTransform parentRectTransform, int startIndex, int length, string word)
     {
         TMP_TextInfo textInfo = textMeshPro.textInfo;
 
@@ -55,40 +46,37 @@ public class CreatePaper : MonoBehaviour
         // Calculate the bounds of the characters in the word
         Bounds bounds = new Bounds(textInfo.characterInfo[startIndex].bottomLeft, Vector3.zero);
 
-        for (int i = 1; i < length; i++)
-        {
-            bounds.Encapsulate(textInfo.characterInfo[startIndex + i].bottomRight);
-            bounds.Encapsulate(textInfo.characterInfo[startIndex + i].topLeft);
-            bounds.Encapsulate(textInfo.characterInfo[startIndex + i].topRight);
-        }
+        bounds.Encapsulate(textInfo.characterInfo[startIndex + length - 1].bottomRight);
+        bounds.Encapsulate(textInfo.characterInfo[startIndex + length - 1].topLeft);
+        bounds.Encapsulate(textInfo.characterInfo[startIndex + length - 1].topRight);
 
         // Create a BoxCollider2D for the word
-        RectTransform wordRectTransform = new GameObject(targetWord + " Transform").AddComponent<RectTransform>();
+        RectTransform wordRectTransform = new GameObject(word + " Transform").AddComponent<RectTransform>();
         wordRectTransform.SetParent(parentRectTransform);
         wordRectTransform.localPosition = bounds.center;
         wordRectTransform.sizeDelta = new Vector2(bounds.size.x, bounds.size.y);
 
         Vector3 dropBoxPos = new Vector3(wordRectTransform.position.x, wordRectTransform.position.y, 0);
 
-        //Create a DropBox here??
         GameObject currGO = Instantiate(dropBox, dropBoxPos , dropBox.transform.rotation);
-
         currGO.transform.SetParent(gameObject.transform);
 
-        DropBox currDropBox = currGO.GetComponent<DropBox>();
+        //currGO.transform = 
 
-        // Create a BoxCollider2D
+        DropBox currDropBox = currGO.GetComponent<DropBox>();
         BoxCollider2D boxCollider = currDropBox.GetComponent<BoxCollider2D>();
 
         // Set the size and position of the BoxCollider2D based on the RectTransform of the target word
         boxCollider.size = new Vector2(wordRectTransform.rect.width, wordRectTransform.rect.height);
-
-        return wordRectTransform;
     }
 
     private string GetReplacementWord(string wordType)
     {
-        return "Testing";
+
+        // TODO: Add new replacement word to the list
+        string newWord = "te" + wordType;
+        targetWords.Add(newWord);
+        return newWord;
     }
 
     private void FindAndReplaceWord()
@@ -103,14 +91,18 @@ public class CreatePaper : MonoBehaviour
             sentence = regex.Replace(sentence, (match) =>
             {
                 string placeholder = match.Groups[1].Value; // Get the text inside the brackets
+                int index = match.Index;
                 string replacement = GetReplacementWord(placeholder);
                 return replacement;
             });
         }
 
-
         textMeshPro.text = sentence;
 
+        for (int i = 0; i < targetWords.Count; i++)
+        {
+            GetWordLocation(targetWords[i]);
+        }
 
     }
 
@@ -119,7 +111,6 @@ public class CreatePaper : MonoBehaviour
         yield return new WaitForSeconds(waitTime);
         //Your Function You Want to Call
         FindAndReplaceWord();
-        GetWordLocations();
 
     }
 }
